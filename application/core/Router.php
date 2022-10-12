2bda15b570784faa84892a1add08a3bdf8bd134e
TomasObrant <dimkl2411@yandex.ru>
2022-10-12 21:04:18 +0300

    first commit

---
+++ application/core/Router.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace application\core;
+
+class Router {
+
+    protected $routes = [];
+    protected $params = [];
+
+    public function __construct() {
+        $arr = require 'application/config/routes.php';
+        foreach ($arr as $key => $val) {
+            $this->add($key, $val);
+        }
+    }
+
+    public function add($route, $params) {
+        $route = '#^'.$route.'$#';
+        $this->routes[$route] = $params;
+    }
+
+    public function match() {
+        $url = trim($_SERVER['REQUEST_URI'], '/');
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function run() {
+        if ($this->match()) {
+            $controller = 'application\controller\\'.ucfirst($this->params['controller']).'Controller.php';
+            if (class_exists($controller)) {
+                echo 'OK';
+            } else {
+                echo 'Не найден: '.$controller;
+            }
+        } else {
+            echo 'Маршрут не найден';
+        }
+    }
+
+
+
+}
